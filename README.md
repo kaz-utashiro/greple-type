63e38c194897c8d4347351209213a1f2c9b4d8d8
Kazumasa Utashiro <kaz@utashiro.com>
2021-07-02 13:33:31 +0900

    minil new App::Greple::gre

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+App::Greple::gre - It's new $module
+
+# SYNOPSIS
+
+    use App::Greple::gre;
+
+# DESCRIPTION
+
+App::Greple::gre is ...
+
+# LICENSE
+
+Copyright (C) Kazumasa Utashiro.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Kazumasa Utashiro <kaz@utashiro.com>
---
+++ lib/App/Greple/gre.pm
@@ -0,0 +1,39 @@
+package App::Greple::gre;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+App::Greple::gre - It's new $module
+
+=head1 SYNOPSIS
+
+    use App::Greple::gre;
+
+=head1 DESCRIPTION
+
+App::Greple::gre is ...
+
+=head1 LICENSE
+
+Copyright (C) Kazumasa Utashiro.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Kazumasa Utashiro E<lt>kaz@utashiro.comE<gt>
+
+=cut
+
